757ce8bb7d05e57fcc31ad0d547cb0e67eb900a2
SCNaves <saintclairnaves@gmail.com>
2024-02-21 16:42:51 -0300

    Chamando a função como void e juntando as modificações do vetor não utilizado

--- Semana 1/MyBFS.py
+++ Semana 1/MyBFS.py
@@ -40,14 +40,7 @@ def cost_function(initial_city, destiny_city, List_of_nodes, qtde_cities):
            
         # Excluir as cidades já visitadas do vetor city_vec
         city_vec = [city for city in city_vec if city not in cities_visited]
-        """
-        # Removendo a cidade atual para que ela não seja uma próxima opção ao longo do algoritmo
-        if i > 1:
-            if cities_visited in city_vec:
-                idx = city_vec.index(cities_visited)   # Pegando o index correspondente a cidade atual
-                city_vec.remove(cities_visited)    # Eliminando a cidade do vetor de opções
-                cost_vec.pop(idx)    # Eliminando o custo do vetor de opções
-        """
+        
         # Aqui é retirado o index associado ao menor custo contido no vetor 'cost_vec'. 
         # Dessa maneira, é possível referenciar o index a 'city_vec' e determinar a cidade associada ao index de menor custo
         lower_cost_idx = cost_vec.index(min(cost_vec))  # index de menor custo
@@ -57,11 +50,9 @@ def cost_function(initial_city, destiny_city, List_of_nodes, qtde_cities):
         cities_visited[i] = actual_city
 
         # Atualiza o valor da variável
-        i = i + 1
+        i = i + 1 
 
-    return cities_visited
-
-# Criando um vetor da quantidade de cidades a serem utilizados no código
+# Criando um variavel da quantidade de cidades a serem utilizados no código
 qtde_cities = 12
 
 # Vetor para conter cidades ja visitadas (possui um tamanho inicial do n° de cidades existentes)
@@ -98,9 +89,9 @@ G = nx.Graph()
 for city in List_of_nodes.keys():   # 'keys()' é utilizada para retirar as primeiras cidades associadas ao dicionário em 'List_of_nodes'
     G.add_node(city)
 
-for city, cost in List_of_nodes.items():    # 'items()' é utilizado para retirar os valores associados a cada cidade no dicionário
-    for connection in cost:
-        G.add_edge(city,connection[0])
+for key, value in List_of_nodes.items():    # 'items()' é utilizado para retirar os valores associados a cada cidade no dicionário
+    for connection in value:
+        G.add_edge(key,connection[0])
 
 # Plotar o grafo
 nx.draw(G, with_labels=True, font_weight='bold')
@@ -112,4 +103,4 @@ initial_city = input()
 print("Digite a cidade de destino: ")
 destiny_city = input()
 
-cidades_visitadas = cost_function(initial_city, destiny_city, List_of_nodes, qtde_cities)
+cost_function(initial_city, destiny_city, List_of_nodes, qtde_cities)
